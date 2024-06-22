cfceb73a4976c24683872cd414070c2b9064d999
dwvwdv <dwvwdv@protonmail.com>
2024-06-22 17:17:30 +0800

    feat: update config.

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,13 @@
+
+return {
+	"github/copilot.vim",
+	event = "VeryLazy",
+	config = function()
+		-- require("copilot").setup()
+	end,
+	keys = {
+		-- {'<SPACE>ai', ':ChatGPT<CR>'},
+	},
+	-- lazy = true,
+	-- enabled = false
+}
